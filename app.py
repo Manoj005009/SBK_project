d062fb7ef7b8d8dc9c133082bc950ba3540a7bd9
MANOJ M <narutouzumaki637491@gmail.com>
2026-02-17 10:25:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify, url_for
-import psycopg2
+import psycopg
 import os
 
 app = Flask(__name__)
@@ -81,4 +81,5 @@ def submit(token):
 
 if __name__ == "__main__":
     app.run(debug=False)
+
 
